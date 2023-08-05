93a6eb4d0a2267e4035cfa5f3bd6004dfd15356f
brunoNabarrete <brunonabarrete@hotmail.com>
2023-08-05 00:19:24 -0300

    adiciono os components inicias que irei utilizar

---
+++ portfolio-brunonabarrete/src/app/components/aboutMe/index.jsx
@@ -0,0 +1,17 @@
+export default function about() {
+    return(
+        <>
+            <div>
+                <h2>
+                    Olá,
+                </h2>
+                <h2>
+                    Meu nome é Bruno Nabarrete,
+                </h2>
+                <h2>
+                    Desenvolvedor FullStack.
+                </h2>
+            </div>
+        </>
+    )
+}
